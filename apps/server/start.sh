959e1d8366aad372a3be30395d23a8ef08dc3d8b
Lokkisanek <matyod@seznam.cz>
2025-12-10 21:48:14 +0100

    fungujk

--- apps/server/start.sh
+++ apps/server/start.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "[start.sh] Waiting for database"
-until npx prisma db push; do
-	echo "[start.sh] Database not ready, retrying in 2s"
-	sleep 2
-done
+echo "[start.sh] Applying Prisma schema"
+npx prisma db push
 
 echo "[start.sh] Seeding database"
 node prisma/seed.js
